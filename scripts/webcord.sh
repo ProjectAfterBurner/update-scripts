a7ba7f23ba8fa75242c823107ffb5783318c0d84
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-04-27 11:48:29 -0400

    re-write everything (#1)
    * testing a new approach
    
    * rework everything

---
+++ scripts/webcord.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+API="$(get_release SpacingBat3/WebCord)"
+
+armhfurl="https://github.com/SpacingBat3/WebCord/releases/download/v${API}/webcord_${API}_armhf.deb"
+arm64url="https://github.com/SpacingBat3/WebCord/releases/download/v${API}/webcord_${API}_arm64.deb"
