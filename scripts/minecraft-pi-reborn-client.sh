4ff132ef9c541ec94bc60e28cfe145fb95b8b7c9
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-05-08 15:35:34 -0400

    update

--- scripts/minecraft-pi-reborn-client.sh
+++ scripts/minecraft-pi-reborn-client.sh
@@ -3,5 +3,5 @@
 API="$(get_release mobilegmYT/mcpi-reborn-extended)"
 
 allurl=""
-armhfurl="https://github.com/mobilegmYT/mcpi-reborn-extended/releases/download/${API}/minecraft-pi-reborn-client_${API}_armhf.deb"
+armhfurl=""
 arm64url="https://github.com/mobilegmYT/mcpi-reborn-extended/releases/download/${API}/minecraft-pi-reborn-client_${API}_arm64.deb"
--- scripts/webcord.sh
+++ scripts/webcord.sh
@@ -3,5 +3,5 @@
 API="$(get_release SpacingBat3/WebCord)"
 
 allurl=""
-armhfurl="https://github.com/SpacingBat3/WebCord/releases/download/v${API}/webcord_${API}_armhf.deb"
+armhfurl=""
 arm64url="https://github.com/SpacingBat3/WebCord/releases/download/v${API}/webcord_${API}_arm64.deb"
